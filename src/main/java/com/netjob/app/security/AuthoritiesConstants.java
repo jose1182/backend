e5a9c9e9653eda9b330f35b04fe25418b970abba
anrogo <antoniorogo96@gmail.com>
2022-03-18 19:52:45 +0100

    prueba2

--- src/main/java/com/netjob/app/security/AuthoritiesConstants.java
+++ src/main/java/com/netjob/app/security/AuthoritiesConstants.java
@@ -8,9 +8,9 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
-/*
+
     public static final String SPECIALIST = "ROLE_SPECIALIST";
-*/
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
